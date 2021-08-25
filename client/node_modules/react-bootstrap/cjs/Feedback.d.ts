ef617f38550b5826da466c546d205bd0f331fad3
Silviu-Andrei Chivescu <72509057+SilviuAndreiChivescu@users.noreply.github.com>
2021-08-25 14:16:44 +0100

    Delete client/node_modules directory

--- client/node_modules/react-bootstrap/cjs/Feedback.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { AsProp, BsPrefixRefForwardingComponent } from './helpers';
-export interface FeedbackProps extends AsProp, React.HTMLAttributes<HTMLElement> {
-    bsPrefix?: never;
-    type?: 'valid' | 'invalid';
-    tooltip?: boolean;
-}
-declare const Feedback: BsPrefixRefForwardingComponent<'div', FeedbackProps>;
-export default Feedback;
--- client/node_modules/react-bootstrap/cjs/SelectableContext.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { SelectCallback } from './helpers';
-declare const SelectableContext: React.Context<SelectCallback | null>;
-export declare const makeEventKey: (eventKey?: string | number | null | undefined, href?: string | null) => string | null;
-export default SelectableContext;
