6690c8342efe6aa4728050b9b00f685d813dfcb7
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-09 14:11:22 +0100

    testing

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const app = express();
 const ProductsModel = require("./models/Products");
 const OrdersModel = require("./models/Orders");
 const UsersModel = require("./models/Users");
-
+// AM RAMAS AICI, CAN''T ACCESS ENV VARIABLE FROM HEROKU, THE MONGODB PASS, DELETE THIS ROW WITHOUT SAVING
 app.use(express.json());
 app.use(cors());
-const mongoPassword = process.env.REACT_APP_MONGODB_URI;
+const mongoPassword = process.env.MONGODB_URI;
 // Change this process.env with mongoDbPassword from above if using for local host
 mongoose.connect(mongoPassword, {
   useNewUrlParser: true,
