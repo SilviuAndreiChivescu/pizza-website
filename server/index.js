79905b9aaae1476dcef90dccb4ee38f917b43562
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-09 13:35:25 +0100

    Changed PORT

--- server/index.js
+++ server/index.js
@@ -355,7 +355,7 @@ app.post("/sendEmail", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello from express");
 });
-
-app.listen(3001, () => {
-  console.log(`Server running on port 3001`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
