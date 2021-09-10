88b9731dc411508e1f05f88c5bcf892ad8346ec8
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-10 15:00:03 +0100

    Deletes GMAIL API

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ const UsersModel = require("./models/Users");
 
 app.use(express.json());
 app.use(cors());
-console.log(typeof process.env.MONGODB_URI);
-console.log(process.env.MONGODB_URI);
+
 // Take password from .env
 const password = process.env.MONGODB_URI;
 const mongoURL = `mongodb+srv://Andrew:${password}@medieval.zxguo.mongodb.net/medieval?retryWrites=true&w=majority`;
@@ -212,150 +211,22 @@ app.put("/updateUsers", async (req, res) => {
 
 // **** END OF USERS COLLECTION ****
 
-// **** GMAIL API ****
-/**
- * @license
- * Copyright Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START gmail_quickstart]
-const fs = require("fs");
-const readline = require("readline");
-const { google } = require("googleapis");
-
-const MIMEText = require("mimetext");
-
-// ** AUTHENTICATION **
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/gmail.modify"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = "token.json";
-
-// Load client secrets from a local file.
-fs.readFile("credentials.json", (err, content) => {
-  if (err) return console.log("Error loading client secret file:", err);
-  // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), sendMessage);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question("Enter the code from that page here: ", (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-// ** END AUTHENTICATION **
-
-var count = 0;
-// Send message
-function sendMessage(auth) {
-  const gmail = google.gmail({ version: "v1", auth });
-  gmail.users.messages
-    .send({
-      userId: "me",
-      requestBody: {
-        raw: message.asEncoded(),
-      },
-    })
-    .then((result) => {
-      // result.id
-      console.log(result.status);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-// Incep pe branchu asta, online branch, cu Mailjet
+// **** MAILJET ****
 
 // Create post request to send message
-// Create message
-const message = new MIMEText();
-message.setSender("medievalpizzacomanda@gmail.com");
-message.setRecipient("gypandy00@gmail.com");
-message.setSubject("Comanda");
-message.setMessage("THE RESTART MESSAGE");
 app.post("/sendEmail", (req, res) => {
   // text variable to store the message passed from front-end
   const text = req.body.text;
   // Set email message to the text passed via request
-  message.setMessage(text);
 
   // Send message
-
-  // Load client secrets from a local file.
-  fs.readFile("credentials.json", (err, content) => {
-    if (err) return console.log("Error loading client secret file:", err);
-    // Authorize a client with credentials, then call the Gmail API.
-    // authorize(JSON.parse(content), sendMessage);
-  });
 
   // Testing purpose
   console.log(`Email ${count++} sent successfully`);
   res.json({ status: "Email sent" });
 });
-// [END gmail_quickstart]
 
-// *** END GMAIL API ***
+// *** END MAILJET ***
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
