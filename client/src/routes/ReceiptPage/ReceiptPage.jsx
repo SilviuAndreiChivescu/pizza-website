85e50417aceb7e1dab7f585971ce7b96d54d2106
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-11 15:02:12 +0100

    If pressing refresh after seeing receipt or going to path '/receipt' without ordering, redirect to '/' - home page

--- client/src/routes/ReceiptPage/ReceiptPage.jsx
+++ client/src/routes/ReceiptPage/ReceiptPage.jsx
@@ -1,19 +1,25 @@
+import { useBeforeunload } from "react-beforeunload";
 import { useTotalNoOfProductAndTotalPrice } from "../../AppLogic";
 import Details from "../../shared components/Details";
 
 export default function ReceiptPage(props) {
   const { lastOrder, lastOrderTime } = props;
   const { totalPrice } = useTotalNoOfProductAndTotalPrice(lastOrder);
-  return (
-    <>
-      <h1>Receipt page</h1>
-      <Details
-        title={"Detalii comanda"}
-        cart={lastOrder}
-        totalPrice={totalPrice}
-      >
-        <p>Data: {lastOrderTime}</p>
-      </Details>
-    </>
-  );
+
+  // If pressing refresh after seeing receipt or going to path '/receipt' without ordering, redirect to '/' - home page
+  if (!lastOrderTime) window.location.replace("http://localhost:3000/");
+  else {
+    return (
+      <>
+        <h1>Receipt page</h1>
+        <Details
+          title={"Detalii comanda"}
+          cart={lastOrder}
+          totalPrice={totalPrice}
+        >
+          <p>Data: {lastOrderTime}</p>
+        </Details>
+      </>
+    );
+  }
 }
