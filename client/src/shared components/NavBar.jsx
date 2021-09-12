f5c4ff4688cee760b95330718541e79d0b988cd5
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-12 16:34:36 +0100

    Adds react-bootstrap UI

--- client/src/shared components/NavBar.jsx
+++ client/src/shared components/NavBar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import MyModal from "./MyModal";
 import Drawer from "./Drawer";
 import { Link } from "react-router-dom";
+import { Col, Container, Navbar, Card } from "react-bootstrap";
 
 export default function NavBar(props) {
   // Destructuring props
@@ -22,39 +23,46 @@ export default function NavBar(props) {
   };
 
   return (
-    <header>
-      <div className="black-bg container-fluid d-inline-flex justify-content-between pe-3 ps-3 text-white">
-        <div className="mt-2">
-          {/* If on NoCartPage, don't render the Arrow because it's home page. If on any other page, render the arrow with the setPageState to go back one page (specified in <NavBar setPageState={() => setPageState("PreviousName")} /> ) */}
-          {to ? <Arrow /> : null}
-          <a
-            href="https://www.facebook.com/pizzamedievalmangalia/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-lg fa-facebook me-2 text-white"></i>
-          </a>
-          <a
-            href="https://www.instagram.com/medievalpizzamangalia/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-lg fa-instagram me-2 text-white"></i>
-          </a>
-          <a href="tel:0754911062">
-            <i className="fas fa-lg fa-phone-square me-2 text-white"></i>
-          </a>
-          <i
-            style={{ cursor: "pointer" }}
-            className="fas fa-lg fa-info-circle text-white"
-            onClick={() => setShow(true)}
-          ></i>
-        </div>
-        <h5 style={{ cursor: "default" }} className="d-inline p-2 ms-5">
-          {title ? title : "Orice comanda este confirmata in maxim 5 minute."}
-        </h5>
-        <Drawer Icon={<i className="fas fa-lg text-white fa-bars ms-5"></i>} />
-      </div>
+    <Container fluid className="p-0">
+      <Navbar className="black-bg container-fluid">
+        <Col sm={1} lg={4} className="text-start ms-2">
+          <h5>
+            {/* Render the Arrow only if the "to" path is specified */}
+            {to ? <Arrow /> : null}
+            <a
+              href="https://www.facebook.com/pizzamedievalmangalia/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab  fa-facebook m-1 text-white"></i>
+            </a>
+
+            <a
+              href="https://www.instagram.com/medievalpizzamangalia/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-instagram m-1 text-white"></i>
+            </a>
+            <a href="tel:0754911062">
+              <i className="fas fa-phone-square m-1 text-white"></i>
+            </a>
+            <i
+              style={{ cursor: "pointer" }}
+              className="fas fa-info-circle text-white m-1"
+              onClick={() => setShow(true)}
+            ></i>
+          </h5>
+        </Col>
+        <Col className="text-center">
+          <Navbar.Brand style={{ cursor: "default" }} className="text-white">
+            {title ? title : "Orice comanda este confirmata in maxim 5 minute."}
+          </Navbar.Brand>
+        </Col>
+        <Col className="text-end">
+          <Drawer Icon={<i className="fas fa-lg text-white fa-bars"></i>} />
+        </Col>
+      </Navbar>
       <MyModal
         Name="Despre noi"
         onClose={() => setShow((currShow) => !currShow)}
@@ -63,43 +71,38 @@ export default function NavBar(props) {
         {/* ModalContent is locally and can be found below this function */}
         <ModalContent />
       </MyModal>
-    </header>
+    </Container>
   );
 }
 
 const ModalContent = () => {
   return (
     <>
-      <div className="m-4 p-4 shadow bg-white">
-        <div className="border-bottom">
-          <p>Contact</p>
-        </div>
-        <div style={{ lineHeight: "1.2" }} className="pt-2">
-          <p className="fw-bold">Pizzeria Medieval</p>
-          <p className="">
+      <Card className="mt-2 mb-4 shadow border-1">
+        <Card.Body>
+          <Card.Title>Pizzeria Medieval</Card.Title>
+          <Card.Text>
             Str. Petru Maior 20, Mangalia, 905500(In spatele Scolii nr.2)
-          </p>
-          <a className="text-decoration-none text-dark" href="tel:0754911062">
-            <p>
+          </Card.Text>
+          <Card.Text>
+            <a className="text-decoration-none text-dark" href="tel:0754911062">
               <i className="fas fa-lg fa-phone-square me-2"></i>0754 911 062
-            </p>
-          </a>
-          <a className="text-decoration-none text-dark" href="tel:0790649803">
-            <p>
+            </a>
+          </Card.Text>
+          <Card.Text>
+            <a className="text-decoration-none text-dark" href="tel:0790649803">
               <i className="fas fa-lg fa-phone-square me-2"></i>0790 649 803
-            </p>
-          </a>
-        </div>
-      </div>
-      <div className="m-4 p-4 shadow bg-white">
-        <div className="border-bottom">
-          <p>Orar de livrari</p>
-        </div>
-        <div className="pt-2">
-          <p>Luni-Duminica: 9:00-23:00</p>
-        </div>
-      </div>
-      <div className="moadalInfo-footer"></div>
+            </a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card className="mb-2 shadow ">
+        <Card.Body>
+          <Card.Title>Orar de livrari</Card.Title>
+          <Card.Text>Luni-Duminica: 9:00-23:00</Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 };
