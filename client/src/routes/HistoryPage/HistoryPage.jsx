21f7116657e08d1ea681ddabbd60afd29abdea34
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-14 09:25:38 +0100

    Adds UI

--- client/src/routes/HistoryPage/HistoryPage.jsx
+++ client/src/routes/HistoryPage/HistoryPage.jsx
@@ -1,10 +1,17 @@
+import NavBar from "../../shared components/NavBar";
 import History from "./History";
 
+// AM RAMAS AICI, SA FAC UI AND SHIT, REMEMBER TO PUT NAVBAR AND EVERYTHING. GOODLUCK
 export default function HistoryPage(props) {
-  const { setPageState, setIdOfOrder } = props;
+  const { setIdOfOrder, setNoCartAnimation } = props;
   return (
-    <main className="page slide-in-bck-center">
-      <History setIdOfOrder={setIdOfOrder} setPageState={setPageState} />
+    <main className="page slide-in-right">
+      <NavBar
+        setAnimation={setNoCartAnimation}
+        title={"Istoricul comenzilor"}
+        to={"/"}
+      />
+      <History setIdOfOrder={setIdOfOrder} />
     </main>
   );
 }
