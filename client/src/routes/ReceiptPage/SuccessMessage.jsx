2a3e60e105ae80bd097abcc200fef1898dde823d
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-15 13:50:58 +0100

    Deletes unused comment

--- client/src/routes/ReceiptPage/SuccessMessage.jsx
+++ client/src/routes/ReceiptPage/SuccessMessage.jsx
@@ -1,7 +1,6 @@
 import { Card, Container } from "react-bootstrap";
 import { ImCheckboxChecked } from "react-icons/im";
 
-// AM RAMAS AICI, FACEAM SI EU FRUMOS CU UN ICON SI UN MESAJ CA A FOST ORDERUITA CU SUCCESS
 export default function SuccessMessage() {
   return (
     <Container className="text-center mt-4">
