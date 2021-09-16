932a1ae62983defced75a9c44c599fdd41547b04
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-16 19:22:21 +0100

    Tries to fix iphone issue

--- client/src/routes/NoCartPage/components/CartBar.jsx
+++ client/src/routes/NoCartPage/components/CartBar.jsx
@@ -18,32 +18,34 @@ export default function CartBar(props) {
           position: "-webkit-sticky",
           alignSelf: "flex-start",
         }}
-        className="text-white black-bg justify-content-center position-sticky bottom-0 container-fluid"
+        className="text-white black-bg justify-content-center position-sticky bottom-0 "
       >
-        <Row
-          style={{ width: "100%" }}
-          xs={3}
-          sm={3}
-          md={3}
-          lg={3}
-          xl={3}
-          xxl={3}
-          className="align-items-center"
-        >
-          <Col>
-            <h4>
-              <FaShoppingBag style={{ marginBottom: "0.85vh" }} />
-              &nbsp;
-              {totalQuantity}
-            </h4>
-          </Col>
-          <Col className="text-center">
-            <h4>Cosul tau</h4>
-          </Col>
-          <Col className="text-end">
-            <h4>{totalPrice} lei</h4>
-          </Col>
-        </Row>
+        <Container fluid className="">
+          <Row
+            style={{ width: "100%" }}
+            xs={3}
+            sm={3}
+            md={3}
+            lg={3}
+            xl={3}
+            xxl={3}
+            className="align-items-center"
+          >
+            <Col>
+              <h4>
+                <FaShoppingBag style={{ marginBottom: "0.85vh" }} />
+                &nbsp;
+                {totalQuantity}
+              </h4>
+            </Col>
+            <Col className="text-center">
+              <h4>Cosul tau</h4>
+            </Col>
+            <Col className="text-end">
+              <h4>{totalPrice} lei</h4>
+            </Col>
+          </Row>
+        </Container>
       </Navbar>
     </Link>
   );
