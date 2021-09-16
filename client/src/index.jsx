c2b6cfb9daa42d27be2f9f34d06e29aae1ed7c9e
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-16 16:38:55 +0100

    Updates Redirect URL for netifly

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,57 +5,34 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// I am working on CartPage UI and functionality ATM
-// UI for all pages, finish. Also add NavBar and what else you want to add to them
+// Check for all page, how do they work, if they act accordinagly. Troubleshoot the whole site more or less. recheck for everypage the layout for different devices, phones. Use dev tools.
 
-// Generate pages for the sideNav and put content in them like Privacy policy and termeni si conditii, there is a generator for them; Those are Routes
-// For history page, make that button track order only show when order is made not more than 50 mins ago, if past 50 mins, make a button that takes you to the fb page to rate us
-
-// For later, in NoCartPage, the NavBar for menu, I want its functionality not to take the user to that #. But to filter the values with what he presses on
-// At some point, if not changed so far, put corect copyright for animation from animista
-
-// Solve problem with Loading. Sometime it just stops on loading. Make a catch or something that when not finish to load, reload or smthing
-
-// In relation with below. I am thinking I can put setAppState("loading") everytime I do a request, and in that request's .then, I can setAppState("loaded"). Also, I can put for catch(err) in that request try, setAppState("error") and I can render a text with error please refresh or come back later if not ok
-// TO MANAGE STATE OF THE ENTIRE APP, LOADING ETC -  After all the request do .then or await(check what await does and if it is same as with .then) LOOK AT SS TAKEN
-
-// DELETE pages.js, now is just my mongoDB route from which I get directions to build the rest of the App
-
-// II
-// Make search input for menu after finishing with the db
-// Auth0 redirect, make it in Romanian
-// FoodBox component maybe delete the ingredients, because they are in the modal anyway
-// Optimize for mobile(maybe bootstrap will do it for me)
-// Drawer component is in shared components folder, but it's used only by NavBar which is also in shared components. Think this through
-// You don't need class components, change them if u have any
 // Check if for each setState I have that depends on the prev value, I have that function call like setState(prevState => prevState + 1)
-// Use lazy initiator for useState hooks if u initiate it to a function. If not, that function will be called everytime the state updates(see ss taken on phone)
 
-// Use best practice for fetching with API, make a file where to put the create, get ... and use it by calling that file easily as I have seen in that video "React Interconnection with db"
-// Hide the key for the db as it shoulf if I should
-// Change boostrap 5 with react-bootstrap CDN or something
-// Redux(read a lot about this and I think i fit in the situation where I don't really need it in my App, this might change later)
 // Clean index.html
 // Delete all non used components & rename all components acordinagly
 
-// See if cata can do with emails like this, to only show a number on tab, or he needs a push up notification. If so, maybe try with another email, try from yahoo or smthing, like to receive
 // Check if there are npm modules unused and uninstall them
 // Unit tests?
 
-// GOOD TO KNOW
-// Contex.API it's ok only if using a context sharing a single value only due to re rendering, see if it may be helpful down the project
-// when using setState for let's say, increment by 1, don't just put setCount(count+1), !!put setCount(currCount => currCount + 1)
-// ALSO for when toggling from false to true, use setState(currState => !currState)
-// When using useEffect and in the dependency array you will put an object, due to referencing of that obj, it might give an unexpected result. SO when using useEffect and want to be dependent of an object, use useMemo() [look it up if needed]
-// Cica useEffect daca nu i dai nici un argument, o sa dea run de fiecare data cand un state din componenta respectiva se va schimba.
-// <React.strictmode> makes ur code render two times at the begging
-// TS is very good for maintaining application and is not too much to learn
+// more states in the same place that depend on each other, could as well be an object
+// const [formData, setFormData] = useState({
+//   uuid: '',
+//   firstName: '',
+//   lastName: '',
+//   address1: '',
+//   address2: '',
+//   address3: '',
+//   addressState, ''
+// });
+// Update it because you are passing too much props and it s not good
+// setFormData({ ...formData, lastName: value_of_last_name });
 
 ReactDOM.render(
   <Auth0Provider
     domain="dev-6rdeed8o.eu.auth0.com"
     clientId="10Iwn9shn6ROfe3HTASmfmvDJ5l5QHEz"
-    redirectUri="http://localhost:3000/"
+    redirectUri="https://hardcore-poincare-73f729.netlify.app/"
   >
     <Router>
       <App />
@@ -65,6 +42,3 @@ ReactDOM.render(
 );
 
 export default App;
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
