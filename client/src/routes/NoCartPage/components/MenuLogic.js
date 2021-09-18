838be12d9aad3d5bfcb90e60615d5749b996c085
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-18 18:54:54 +0100

    Fixes bug for heroku deployment

--- client/src/routes/NoCartPage/components/MenuLogic.js
+++ client/src/routes/NoCartPage/components/MenuLogic.js
@@ -14,34 +14,36 @@ const useFilteredProductsList = (productsList) => {
 
   // Filter the filteredProductsList for each category
   useEffect(() => {
-    // Filter the entire Products Collection by the value of Search Input Field
-    let filteredList = productsList.filter((e) =>
-      e.Name.toLowerCase().match(filter)
-    );
-    // Filter the pizzas
-    let pizzas = filteredList.filter((e) => e.Category === "pizza");
+    if (productsList && productsList.length !== 0) {
+      // Filter the entire Products Collection by the value of Search Input Field
+      let filteredList = productsList.filter((e) =>
+        e.Name.toLowerCase().match(filter)
+      );
+      // Filter the pizzas
+      let pizzas = filteredList.filter((e) => e.Category === "pizza");
 
-    // Flter the burgers
-    let burgers = filteredList.filter((e) => e.Category === "burgers");
+      // Flter the burgers
+      let burgers = filteredList.filter((e) => e.Category === "burgers");
 
-    // Filter the chifles
-    let chifle = filteredList.filter((e) => e.Category === "chifle");
+      // Filter the chifles
+      let chifle = filteredList.filter((e) => e.Category === "chifle");
 
-    // Filter the sandwichs
-    let sandwich = filteredList.filter((e) => e.Category === "sandwich");
+      // Filter the sandwichs
+      let sandwich = filteredList.filter((e) => e.Category === "sandwich");
 
-    // Filter the drinks
-    let drinks = filteredList.filter((e) => e.Category === "drinks");
+      // Filter the drinks
+      let drinks = filteredList.filter((e) => e.Category === "drinks");
 
-    // Set the state
-    setFood({
-      ...food,
-      pizzas: pizzas,
-      burgers: burgers,
-      chifle: chifle,
-      sandwich: sandwich,
-      drinks: drinks,
-    });
+      // Set the state
+      setFood({
+        ...food,
+        pizzas: pizzas,
+        burgers: burgers,
+        chifle: chifle,
+        sandwich: sandwich,
+        drinks: drinks,
+      });
+    }
   }, [filter]);
 
   return { setFilter, food };
