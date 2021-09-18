e84849f325db91f603a00ebe1f66c8c86dc3893d
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-18 17:11:44 +0100

    Updates Todo list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,34 +5,21 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// Check for all page, how do they work, if they act accordinagly. Troubleshoot the whole site more or less. recheck for everypage the layout for different devices, phones. Use dev tools.
+// Html lang tag set it to ro and then check if it translates ok to eng for my portf
+// also check what this lang does
 
-// Check if for each setState I have that depends on the prev value, I have that function call like setState(prevState => prevState + 1)
+// DO font families with bi
 
-// Clean index.html
-// Delete all non used components & rename all components acordinagly
+// do file structure for whole project for deployment, and I think I will deploy it only on heroku all together after I sort it out how to deploy it for cata
+// think about this for deploying app on same host, you will need your back end at the top, not in server, in order to deploy it on same host server
 
 // Check if there are npm modules unused and uninstall them
-// Unit tests?
-
-// more states in the same place that depend on each other, could as well be an object
-// const [formData, setFormData] = useState({
-//   uuid: '',
-//   firstName: '',
-//   lastName: '',
-//   address1: '',
-//   address2: '',
-//   address3: '',
-//   addressState, ''
-// });
-// Update it because you are passing too much props and it s not good
-// setFormData({ ...formData, lastName: value_of_last_name });
 
 ReactDOM.render(
   <Auth0Provider
     domain="dev-6rdeed8o.eu.auth0.com"
     clientId="10Iwn9shn6ROfe3HTASmfmvDJ5l5QHEz"
-    redirectUri="https://hardcore-poincare-73f729.netlify.app/"
+    redirectUri="http://localhost:3000"
   >
     <Router>
       <App />
