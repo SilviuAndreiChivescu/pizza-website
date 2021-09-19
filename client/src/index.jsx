4c0546e001b47d8b345ef7fb483e501064540cb6
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-19 18:50:02 +0100

    Connects with env vars

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,20 +5,17 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// I deleted procfile since on the article it says if there is no procfiler, heroku looks for start script in package.json, so I have one
-
-// Html lang tag set it to ro and then check if it translates ok to eng for my portf
-// also check what this lang does
-
-// DO font families with bi
-
 // Check if there are npm modules unused and uninstall them
+
+// Add crud for products collection and put it in MVC pattern and maybe do the front end of it, like with react bootstrap easy pez
+
+// readMe
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-6rdeed8o.eu.auth0.com"
-    clientId="10Iwn9shn6ROfe3HTASmfmvDJ5l5QHEz"
-    redirectUri={"https://pizza-website2021.herokuapp.com/"}
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENTID}
+    redirectUri={process.env.REACT_APP_FRONTEND}
   >
     <Router>
       <App />
