29c963ece274b44ce99d7697758b92cd55008fa6
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-20 20:14:31 +0100

    Cleans up code by dividing in multiple files

--- client/src/routes/AdminPage/AdminPage.jsx
+++ client/src/routes/AdminPage/AdminPage.jsx
@@ -1,133 +1,32 @@
-import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import MyNavbar from "../../shared components/MyNavbar";
 
-import {
-  useProductsStates,
-  useAddToProducts,
-  useUpdateProducts,
-  useDeleteProduct,
-  useProductsList,
-} from "./AdminLogic";
+import { useProductsList } from "./AdminPageLogic";
 
-import { Col, Container, Form, Row, Button } from "react-bootstrap";
+import NewProduct from "./components/NewProduct";
+import ProductsList from "./components/ProductsList";
 
-// TODO, MAKE THIS FILE A Admin.jsx FILE THAT LIVES IN ADMINPAGE.JSX WHERE I CAN PUT NAVBAR AS WELL
-export default function AdminPage() {
-  // Products States
-  const { productsStates, setProductsStates } = useProductsStates();
+export default function AdminPage(props) {
+  // Verify if user is auth and if is admin, otherwise send to home page
+  const { user, isAuthenticated } = useAuth0();
+  if (!isAuthenticated || user.email !== "gypandy00@gmail.com")
+    window.location.replace("/");
+
+  const { setNoCartAnimation } = props;
 
   // Read from Products Collection
   const { productsList, setProductsList } = useProductsList();
 
-  // Add to Products Collection
-  const { addToProducts } = useAddToProducts(productsList, setProductsList);
-
-  // Update Products Collection by id
-  const { updateProducts, newProduct, setNewProduct } = useUpdateProducts();
-
-  // Delete product from Products Collection by id
-  const { deleteProduct } = useDeleteProduct();
-
   return (
-    <Container className="text-center">
-      <h1>Admin Page</h1>
-      <br></br>
+    <main className="slide-in-right">
+      <MyNavbar title="Admin" to="/" setAnimation={setNoCartAnimation} />
+      <NewProduct
+        productsList={productsList}
+        setProductsList={setProductsList}
+      />
 
-      <Row>
-        <Col>
-          <Form.Control
-            placeholder="Product name"
-            onChange={(event) => {
-              setProductsStates({
-                ...productsStates,
-                name: event.target.value,
-                // Making the name from 'Pizza Medieval' to 'pizza_medieval'
-                imageUrl: event.target.value.toLowerCase().split(" ").join("_"),
-              });
-            }}
-          />
-        </Col>
-        <Col>
-          <Form.Control
-            placeholder="Category"
-            onChange={(event) => {
-              setProductsStates({
-                ...productsStates,
-                category: event.target.value,
-              });
-            }}
-          />
-        </Col>
-      </Row>
-
-      <Row className="mt-2 mb-2">
-        <Col>
-          <Form.Control
-            placeholder="Price"
-            type="number"
-            onChange={(event) => {
-              setProductsStates({
-                ...productsStates,
-                price: event.target.value,
-              });
-            }}
-          />
-        </Col>
-        <Col>
-          <Form.Control
-            placeholder="ImageName"
-            value={productsStates.imageUrl}
-            onChange={(event) => {
-              setProductsStates({
-                ...productsStates,
-                imageUrl: event.target.value,
-              });
-            }}
-          />
-        </Col>
-      </Row>
-      <Col>
-        <Form.Control
-          as="textarea"
-          rows={4}
-          placeholder="Description"
-          onChange={(event) => {
-            setProductsStates({
-              ...productsStates,
-              description: event.target.value,
-            });
-          }}
-        />
-      </Col>
-
-      <Button
-        className="mt-3"
-        variant="outline-dark"
-        onClick={() => addToProducts(productsStates)}
-      >
-        Add to List
-      </Button>
-      <h1>Products List</h1>
-
-      {productsList.map((val, key) => {
-        return (
-          <div key={key}>
-            <h1>
-              {val.Name} for {val.Price}
-            </h1>
-            <input
-              onChange={(event) =>
-                setNewProduct({ ...newProduct, name: event.target.value })
-              }
-              type="text"
-              placeholder="New product name"
-            />
-            <button onClick={() => updateProducts(val._id, newProduct)}>
-              Update
-            </button>
-            <button onClick={() => deleteProduct(val._id)}>Delete</button>
-          </div>
-        );
-      })}
-    </Container>
+      <ProductsList productsList={productsList} />
+    </main>
   );
 }
