514a3e6a077472c75ef719c5c0756c8b65799ecf
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-20 20:13:58 +0100

    Finishes Function

--- src/service/Products.js
+++ src/service/Products.js
@@ -28,11 +28,34 @@ const InsertIntoProducts = async (product) => {
   }
 };
 
-// I have not tested this and below it yet but I think it works properly
 const UpdateProducts = async (id, newProduct) => {
+  // Firstly it is checked how many options the product has and makes the sizes in the priceArray
+  if (
+    newProduct.price !== 0 &&
+    newProduct.price2 !== 0 &&
+    newProduct.price3 !== 0
+  ) {
+    var priceArray = new Array(
+      parseFloat(newProduct.price),
+      parseFloat(newProduct.price2),
+      parseFloat(newProduct.price3)
+    );
+  } else if (newProduct.price !== 0 && newProduct.price2 !== 0) {
+    var priceArray = new Array(
+      parseFloat(newProduct.price),
+      parseFloat(newProduct.price2)
+    );
+  } else if (newProduct.price !== 0)
+    var priceArray = new Array(parseFloat(newProduct.price));
+
   try {
     await ProductsModel.findById(id, (err, updatedProduct) => {
       updatedProduct.Name = newProduct.name;
+      updatedProduct.Price = priceArray;
+      updatedProduct.Quantity = newProduct.quantity;
+      updatedProduct.Description = newProduct.description;
+      updatedProduct.Image = newProduct.imageUrl;
+      updatedProduct.Category = newProduct.category;
       updatedProduct.save();
     });
   } catch (err) {
