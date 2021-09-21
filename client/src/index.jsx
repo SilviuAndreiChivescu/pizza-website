1a06bb409f7db6dee1a93d5db509a2db219cfd2a
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-21 10:28:12 +0100

    Adds todo on list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,10 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 // Check if there are npm modules unused and uninstall them
 
-// Add crud for products collection and put it in MVC pattern and maybe do the front end of it, like with react bootstrap easy pez
+// pt portf meu, sa mai sterg din pizze, lasa mai putine. Also think what other things there are, like making for cata all his account and his db, to not be bothered to not delete stuff.
+// also translate all romanian stuff to english for prtf
+
+// AM RAMAS AICI, MAI GANDESTE TE DACA MAI TRB FACUT CEVA LA ADMIN FUNCITONALITY ASTA, DACA E GATA, POTI SA II DAI MERGE LA DEV SI POATE DUPA SI LA MASTER
+// test receipt and adminpage online, see if they redirect how they should to home page since i changed the env var from link to '/'
 
 // readMe
-
 ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_DOMAIN}
